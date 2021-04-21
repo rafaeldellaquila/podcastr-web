a0f1a460b0a4b54c5e28e914d1afefae2689d342
rafaeldellaquila <rafadellaquila@gmail.com>
2021-04-20 21:57:11 -0300

    :sparkles: add header component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,18 +1,11 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
-
 import styles from './styles.module.scss';
 
 export default function Header() {
-  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-    locale: ptBR,
-  });
-
   return (
     <header className={styles.headerContainer}>
       <img src='/logo.svg' alt='Podcastr logo' />
       <p>O melhor para você ouvir, sempre.</p>
-      <span>{currentDate}</span>
+      <span>a</span>
     </header>
   );
 }
